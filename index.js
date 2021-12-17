c39f0eddd01f390819e589bcda2525037c75907e
AJ Jalusi <ajalusi26@gmail.com>
2021-12-17 14:01:15 -0500

    Comleted assignment

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // Write your code here!
 
+
+
 main.remove();
 
 const newHeader = document.getElementById('victory')
 
 newHeader.textContent = "AJ Jalusi is the champion";
+
+const element = document.createElement('div');
+document.body.append(div)
+const ul = document.createElement("ul");
+
+for (let i =0; i<3; i++){
+    const li = document.createElement('li')
+    li.textContent = (i+1).toString();
+    ul.append(li)
+}
+element.append(ul);
